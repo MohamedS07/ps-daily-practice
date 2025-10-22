ecb2e1ea038f794519e614cca44273ab9bebf83a
Mohamed idhrish <mohamedidhrish215@gmail.com>
2025-10-22 13:08:51 +0530

    created a new file

--- worksheet_10.py
+++ worksheet_10.py
@@ -73,12 +73,7 @@ space_remove('Welcome to the world')
 
 # Write a Python program that asks the user to enter a password and checks if it is strong. A password is considered strong if:
 
-password = input("Enter your password: ")
-if len(password) >= 8 :
-    if ("!" in password or "@" in password or "#" in password or "$" in password or "%" in password or "^" in password or "&" in password or "*" in password):
-        print("Password is strong")
-    else:
-        print("Password is not strong")
+
 
 
 
